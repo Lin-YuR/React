521873136ab6fb997d1e1164856b73995b411e75
Yu, Lin <40156908+Lin-YuR@users.noreply.github.com>
2018-07-24 12:34:18 +0800

    Create App.js

---
+++ InputText/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import TextInput from './TextInput'
+
+class App extends React.Component {
+  render() {
+    return <TextInput initText="Type something here!" />
+  }
+}
+export default App
