df4a8fed1c9ace7b2565a8ce19fed3545df2ab78
Yu, Lin <40156908+Lin-YuR@users.noreply.github.com>
2018-07-25 14:38:09 +0800

    Update index.js

--- TodoList/index.js
+++ TodoList/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+import TodoList from './reactmeow/TodoList';
 
 ReactDOM.render(
-  <App />,document.getElementById('root')
+  <TodoList />,document.getElementById('root')
 )
