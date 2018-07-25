cb3ce4101de88603e5588b0e214f044eee9e4fea
Yu, Lin <40156908+Lin-YuR@users.noreply.github.com>
2018-07-25 14:42:18 +0800

    Delete App.js

--- TodoList/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import TextInput from './TodoList'
-
-class App extends React.Component {
-  render() {
-    return <TextInput initText="Type something here!" />
-  }
-}
-export default App
