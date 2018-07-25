eb4a94f6d2d0297b14e14fc0f1246afc91d6f45a
Yu, Lin <40156908+Lin-YuR@users.noreply.github.com>
2018-07-25 14:41:57 +0800

    Create TodoList.js

---
+++ TodoList/reactmeow/TodoList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+class TodoList extends React.Component{
+    state = {
+        items:[],
+        inputValue:'',
+    }
+    handleChange = (e) =>{
+        this.setState({inputValue: e.target.value})
+    }
+    handleKeyPress = (e) =>{
+        if(e.key === 'Enter'){
+            const newItems = [e.target.value, ...this.state.items]
+
+            this.setState({
+                items: newItems,
+                inputValue:'',
+            })
+        }
+    }
+    render(){
+        return(
+            <div>
+                <input 
+                  placeholder = 'type something here!'
+                  onChange = {this.handleChange}
+                  onKeyPress = {this.handleKeyPress}
+                />
+                <ul>
+                {
+                    this.state.items.map((value)=>{return <li>{value}</li>})
+                }
+                </ul>
+            </div>
+        )
+    }
+}
+export default TodoList
