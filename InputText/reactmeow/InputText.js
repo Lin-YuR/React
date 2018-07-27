4fd076cef18598f8e3ca9f05975d884e0be776b7
Yu, Lin <40156908+Lin-YuR@users.noreply.github.com>
2018-07-27 14:06:27 +0800

    Update InputText.js

--- InputText/reactmeow/InputText.js
+++ InputText/reactmeow/InputText.js
@@ -1,18 +1,24 @@
 import React from 'react'
 
 class InputText extends React.Component{
-    state = {text:'',}
-    handleChange = (e) =>{
-      this.setState({text: e.target.value})
-    }
-    render(){
-      return(
-        <div>
-          <input
-          placeholder='Type something here!' 
-          onChange={this.handleChange}/>
-          <h1>{this.state.text}</h1>
-        </div>
+  
+  constructor(props){
+    super(props)
+    this.state = {text:'',}
+  }
+
+  handleChange = (e) =>{
+    this.setState({text: e.target.value})
+  }
+
+  render(){
+    return(
+      <div>
+        <input
+        placeholder='Type something here!' 
+        onChange={this.handleChange}/>
+        <h1>{this.state.text}</h1>
+      </div>
       )
     }
   }
